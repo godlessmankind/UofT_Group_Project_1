6dd2689bbbe406aafa87b73756e89fb072b70ebe
suedoh <suedoh36@proton.me>
2023-04-15 18:28:08 -0400

    cleaned up comments are console logs

--- flightUtils.js
+++ flightUtils.js
@@ -8,27 +8,13 @@ function formatOptions(options) {
     return queryString.slice(0, -1)
 }
 
-/* async function fetchFlights(API_KEY, options) {
-    const apiUrl = 'http://api.aviationstack.com/v1/flights'
-    if (!API_KEY) {
-        throw new Error('No API_KEY provided')
-    }
-    const optionQuery = formatOptions(options)
-    const data = await fetch(`${apiUrl}?access_key=${API_KEY}&${optionQuery}`)
-    if (!data.ok) {
-        throw new Error(`HTTP error: ${data.status}`)
-    }
-    const json = await data.json()
-    return json
-}*/
-
 async function fetchFlights(API_KEY, options, departingIataCode) {
   const apiUrl = 'http://api.aviationstack.com/v1/flights'
   if (!API_KEY) {
     throw new Error('No API_KEY provided')
   }
 
-  console.log(options, departingIataCode)
+  // console.log(options, departingIataCode)
   const optionQuery = formatOptions(options, departingIataCode)
   const data = await fetch(`${apiUrl}?access_key=${API_KEY}&${optionQuery}`)
   if (!data.ok) {
